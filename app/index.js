45dc70af1f699d4ef4e8ed95ba6a14da1736ed4e
tolgagalipaslan <tolgagalipaslan1999@gmail.com>
2023-03-27 20:37:10 +0300

    first commit

---
+++ app/index.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+
+import { Text, View, ScrollView, SafeAreaView } from "react-native";
+import { COLORS, images, icons, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+
+const Home = () => {
+  const router = useRouter();
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => {
+            return <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />;
+          },
+          headerRight: () => {
+            return (
+              <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+            );
+          },
+          headerTitle: "index",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ padding: SIZES.medium, flex: 1 }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
