605bdb37fd327d7e93aa94ffa3d1d43a8e1e9c8c
tolgagalipaslan <tolgagalipaslan1999@gmail.com>
2023-03-28 03:51:38 +0300

    api connection

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,50 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './popularjobs.style'
-
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import styles from "./popularjobs.style";
+import { useRouter } from "expo-router";
+import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from "../../../hook/useFetch";
 const Popularjobs = () => {
-  return (
-    <View>
-      <Text>Popularjobs</Text>
-    </View>
-  )
-}
+  const router = useRouter();
 
-export default Popularjobs
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React Developer",
+    num_page: 1,
+  });
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.cardsContainer}>
+        {/* isLoading alttaki knk */}
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          //.map gibi birsi bu sanirim
+          <FlatList
+            data={[1, 2, 3, 4]}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
+            keyExtractor={(item) => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs;
---
+++ hook/useFetch.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { RAPID_API_KEY } from "@env";
+const useFetch = (endpoint, query) => {
+  const keyRapid = RAPID_API_KEY;
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": keyRapid,
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("There is an error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const reFetch = () => {
+    setIsLoading(true);
+
+    fetchData();
+  };
+  return { data, isLoading, error, reFetch };
+};
+
+export default useFetch;
